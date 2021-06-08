e46602a7d4801cb7e51445d4958c319829126f32
ifemmanuel <85588778+ifemmanuel@users.noreply.github.com>
2021-06-08 16:49:20 -0500

    Add files via upload

---
+++ formulario.php
@@ -0,0 +1,13 @@
+<?php
+
+$nom = $_GET['nombre'];
+$eda = $_GET['email'];
+$grup = $_GET['telefono'];
+$calif = $_GET['mensaje'];
+
+echo $nom."<br>";
+echo $eda."<br>";
+echo $grup."<br>";
+echo $calif."<br>";
+
+?>
